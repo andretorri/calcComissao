3368ff316a9a00096bbe1d302818fc333c88c8dc
torresmo <andretorri1@gmail.com>
2025-06-03 22:05:41 -0300

    Implementação do Radio Button e captura do ID Checagem se o Radio Button foi selecionado via logcat

--- app/src/main/java/com/example/conversorUnidades/MainActivity.kt
+++ app/src/main/java/com/example/conversorUnidades/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.conversorUnidades
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,13 +21,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //recuperar o valor do Edit Text
+        //associando os compeonentes do Edit Text
         val edtValue = findViewById<TextInputEditText>(R.id.valor_venda)
         //recuperar a ação do botão
-        val button = findViewById<Button>(R.id.btn_calc)
+        val calculate = findViewById<Button>(R.id.btn_calc)
+        //associando cada Radio Button em variavel
+        val vendaDireta = findViewById<RadioButton>(R.id.vendaDireta)
+        val showRoom = findViewById<RadioButton>(R.id.showRoom)
+        val semiNovo = findViewById<RadioButton>(R.id.semiNovo)
+        val tipoVendas = findViewById<RadioGroup>(R.id.tipoVendas)
 
-        button.setOnClickListener {
-            println("cliquei no botão ${edtValue.text}")
+        //recuperar o valor digitado pelo usuário
+        calculate.setOnClickListener {
+            val edtValue: String = edtValue.text.toString()
+            println(edtValue)
+        }
+        //verificar se radio button foi selecionado
+        findViewById<RadioButton>(R.id.vendaDireta).setOnCheckedChangeListener { buttonView, isChecked ->
+            Log.d("Radio", "Venda Direta $isChecked")
         }
     }
 }
