cfaea466a4923aa30e9b665e65655341127a03c3
torresmo <andretorri1@gmail.com>
2025-06-04 23:04:06 -0300

    Criação do título e do paragrafo de texto Troca do FindViewById pelo ViewBinding Melhoria no layou do RadioButton Checagem de qual RadioButton está selecionado

--- app/src/main/java/com/example/conversorUnidades/MainActivity.kt
+++ app/src/main/java/com/example/conversorUnidades/MainActivity.kt
@@ -9,36 +9,38 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.conversorUnidades.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+    //criando a binding do ViewBinding para retirar o uso do FindViewById
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        //associando os compeonentes do Edit Text
-        val edtValue = findViewById<TextInputEditText>(R.id.valor_venda)
-        //recuperar a ação do botão
-        val calculate = findViewById<Button>(R.id.btn_calc)
-        //associando cada Radio Button em variavel
-        val vendaDireta = findViewById<RadioButton>(R.id.vendaDireta)
-        val showRoom = findViewById<RadioButton>(R.id.showRoom)
-        val semiNovo = findViewById<RadioButton>(R.id.semiNovo)
-        val tipoVendas = findViewById<RadioGroup>(R.id.tipoVendas)
-
         //recuperar o valor digitado pelo usuário
-        calculate.setOnClickListener {
-            val edtValue: String = edtValue.text.toString()
+        //utilizando apenas o ViewBinding
+        binding.btnCalc.setOnClickListener {
+            val edtValue: String = binding.valorVenda.text.toString()
             println(edtValue)
         }
-        //verificar se radio button foi selecionado
-        findViewById<RadioButton>(R.id.vendaDireta).setOnCheckedChangeListener { buttonView, isChecked ->
-            Log.d("Radio", "Venda Direta $isChecked")
+        //recuperar os Radio Buttons pelo Viewbinding
+        binding.vendaDireta.setOnCheckedChangeListener { _, isChecked ->
+            println("Check $isChecked")
+        }
+        binding.showRoom.setOnCheckedChangeListener { _, isChecked ->
+            println("Check2 $isChecked")
+        }
+        binding.semiNovo.setOnCheckedChangeListener { _, isChecked ->
+            println("Check3 $isChecked")
         }
     }
 }
